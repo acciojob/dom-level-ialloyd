8aa2faac69855cf1289f485504128b80c79201d6
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-20 20:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+function findLevel(id) {
+  let level = 1;
+  let element = document.getElementById(id);
+  while (element.parentNode) {
+    element = element.parentNode;
+    level++;
+  }
+  return level;
+}
+
+const n=findLevel("level");
+alert(`The level of the element is: ${n}`);
